c1babbdd9abfaa07940c5ef71b70f9be335a1439
Talita1996 <talita_mota@outlook.com>
2019-06-01 17:27:46 -0300

    jogo sem o minimax

--- hash.py
+++ hash.py
@@ -4,10 +4,10 @@ class Hash:
                       [0, 0, 0],
                       [0, 0, 0]]
 
-    def setBoard(self, symbol, i, j):
+    def set_board(self, symbol, i, j):
         self.board[i][j] = symbol
 
-    def getBoard(self):
+    def get_board(self):
         print('     0 | 1 | 2 ')
         print('    ------------')
 
@@ -17,3 +17,28 @@ class Hash:
                 symbol = self.board[row][column]
                 print(f' {symbol} |', end='')
             print()
+
+    def get_cell(self, i, j):
+        return self.board[i][j]
+
+    def wins(self, player):
+        win_state = [[self.get_cell(0, 0), self.get_cell(0, 1), self.get_cell(0, 2)],
+                     [self.get_cell(1, 0), self.get_cell(1, 1), self.get_cell(1, 2)],
+                     [self.get_cell(2, 0), self.get_cell(2, 1), self.get_cell(2, 2)],
+                     [self.get_cell(0, 0), self.get_cell(1, 0), self.get_cell(2, 0)],
+                     [self.get_cell(0, 1), self.get_cell(1, 1), self.get_cell(2, 1)],
+                     [self.get_cell(0, 2), self.get_cell(1, 2), self.get_cell(2, 2)],
+                     [self.get_cell(0, 0), self.get_cell(1, 1), self.get_cell(2, 2)],
+                     [self.get_cell(0, 2), self.get_cell(1, 1), self.get_cell(2, 0)]]
+
+        if [player, player, player] in win_state:
+            return True
+        else:
+            return False
+
+    def empty_cells(self):
+        for row in range(3):
+            for column in range(3):
+                if self.get_cell(row, column) == 0:
+                    return True
+        return False
--- main.py
+++ main.py
@@ -2,11 +2,25 @@ from hash import Hash
 import os
 
 board = Hash()
-board.getBoard()
+board.get_board()
 
-i = int(input('Digite um número para a linha: '))
-j = int(input('Digite um número para a coluna: '))
+while board.empty_cells():
+    i = int(input('\nDigite um número para a linha: '))
+    j = int(input('Digite um número para a coluna: '))
+    board.set_board('X', i, j)
+    if board.wins('X') or board.wins('o'):
+        print('\nFim de jogo!')
+        break
+        
+    aux = 0
+    for row in range(3):
+        for column in range(3):
+            if board.get_cell(row, column) == 0 and aux == 0:
+                board.set_board('o', row, column)
+                aux = 1
 
-board.setBoard('X', i, j)
-os.system('cls')
-board.getBoard()
+    os.system('cls')
+    board.get_board()
+    if board.wins('X') or board.wins('o'):
+        print('\nFim de jogo!')
+        break
